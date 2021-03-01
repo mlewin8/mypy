bb7888af7b906e3d4e9ef5a27bf32b05f2e9a4ef
mlewin8 <79075999+mlewin8@users.noreply.github.com>
2021-03-01 03:19:03 -0500

    Add files via upload

---
+++ Time.py
@@ -0,0 +1,15 @@
+import sys
+
+for line in sys.stdin:
+
+    data = line.strip().split("\t")
+
+    if len(data) == 6:
+
+        date, time, store, item, cost, payment = data
+
+    print ("{0}\t{1}".format(item, cost))
+    print ("{0}\t{1}".format(date, time))
+
+    def timedelta(seconds=60):
+        
